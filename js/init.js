d88a81c86889e6aac0a60368f7c0213e4d36ddea
Emmanuel Tolani <emmanueltolani17@gmail.com>
2022-05-16 22:51:56 -0600

    Revamped my website

--- js/init.js
+++ js/init.js
@@ -268,7 +268,7 @@ function arlo_tm_animate_text() {
   var animateSpan = jQuery(".arlo_tm_animation_text_word");
 
   animateSpan.typed({
-    strings: ["Front-end Developer", "React Developer", "Email Marketer"],
+    strings: ["Front-end Developer", "React Developer", "Software Developer"],
     loop: true,
     startDelay: 1e3,
     backDelay: 2e3,
